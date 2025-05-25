0ecce6337178d5e3837e14af19eb82e4f75bb067
karthik <karthik986340@gmail.com>
2025-05-25 10:15:10 +0530

    updated ads

--- ads.js
+++ ads.js
@@ -1,8 +1,16 @@
 // Google AdSense Integration
 window.onload = function () {
     // Check if cookies are accepted before loading ads
-    if (getCookie('cookie_consent') === 'accepted') {
-        loadAdsenseScript();
+    try {
+        const consentData = JSON.parse(getCookie('cookie_consent'));
+        if (consentData && consentData.advertising) {
+            loadAdsenseScript();
+        } else {
+            hideAds();
+        }
+    } catch (e) {
+        // If cookie doesn't exist or is invalid, hide ads
+        hideAds();
     }
 };
 
@@ -13,12 +21,8 @@ document.addEventListener('adsEnabled', function () {
 
 // Function to load the Google AdSense script
 function loadAdsenseScript() {
-    // The AdSense script is now loaded in the HTML head
-    // Just show the ads that were hidden
+    // Show the ads that were hidden
     showAds();
-
-    // We don't need to initialize ads here anymore
-    // Each ad unit has its own initialization script in the HTML
     console.log('AdSense containers are now visible');
 }
 
@@ -39,8 +43,6 @@ function showAds() {
             if (!adElement.style.height) {
                 adElement.style.height = '250px';
             }
-
-            // No need to push ads here - they're initialized by inline scripts
         }
     });
 }
